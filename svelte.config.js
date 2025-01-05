f206ae79c6bb99c9867473ec82583e881ec9e687
user <jfarbotn@uottawa.ca>
2025-01-05 16:45:48 -0500

    add configuration for site

--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,12 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		paths: {
+			// https://svelte.dev/docs/kit/adapter-static#GitHub-Pages
+			// https://github.com/sveltejs/kit/discussions/11554
+			// https://github.com/sveltejs/kit/issues/1553
+			base: process.argv.includes("dev") ? "" : "/logic-sim-site",
+		},
 		adapter: adapter({
 			/*
 				The directory to write prerendered pages to. It defaults to build.
@@ -21,7 +27,7 @@ const config = {
 			/*
 				If true, precompresses files with brotli and gzip. This will generate .br and .gz files.
 			*/
-			precompress: true,
+			precompress: false,
 			/*
 				By default, adapter-static checks that either all pages and endpoints (if any) of your app were prerendered, or you have the fallback option set. This check exists to prevent you from accidentally publishing an app where some parts of it are not accessible, because they are not contained in the final output. If you know this is ok (for example when a certain page only exists conditionally), you can set strict to false to turn off this check.
 			*/
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-//import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
-		//topLevelAwait(),
 	],
 	//assetsInclude: ["**/*.wasm"],
 	build: {
